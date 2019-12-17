3a119e56e75f5bf3b6f38b6798eff4998ca57f57
HZ <hzhihhhhhh@163.com>
2019-12-17 10:40:55 +0800

    MyLife first commit all

---
+++ src/com/HZ/MyLife/entity/User.java
@@ -0,0 +1,107 @@
+package com.HZ.MyLife.entity;
+
+import java.util.Date;
+
+/**用户类
+ * Created by xsjdn on 2019/12/6.
+ */
+public class User {
+    private String uid;
+    private String username;
+    private String password;
+    private String telephone;
+    private String email;
+    private String name;
+    private String sex;
+    private Date birthday;
+    private String state;
+    private String code;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
